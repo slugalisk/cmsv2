b3e1a12d0506d2e5cad0450c09ea7f90c2a459b4
Kevin Choi <kevinchoinj@yahoo.com>
2017-11-21 01:51:09 -0500

    each form object in its own div instead of columns in chat options

--- client/src/components/admin/forms/ChatForm.js
+++ client/src/components/admin/forms/ChatForm.js
@@ -11,175 +11,228 @@ const ChatForm = props => {
 
     <Col md={4} mdOffset={0} sm={6} smOffset={0} xs={12} xsOffset={0}> 
       <Col md={10} mdOffset={1} sm={10} smOffset={1} xs={10} xsOffset={1}>
-      <Row className='admin_setup__row'>
-        <label>Combos</label>
-        <Field 
-          name='chatCombos'
-          component='select'
-          className='admin_form_field'
-        >
-          <option />
-          <option value='yes'>Yes</option>
-          <option value='no'>No</option>
-        </Field>
-      </Row>
-
-      <Row className='admin_setup__row'>
-        <label>Overrustle</label>
-        <Field 
-          name='chatOverrustle'
-          component='select'
-          className='admin_form_field'
-        >
-          <option />
-          <option value='yes'>Yes</option>
-          <option value='no'>No</option>
-        </Field>
-      </Row>
-
-      <Row className='admin_setup__row'>
-        <label>Profanity Filter</label>
-        <Field 
-          name='chatProfanity'
-          component='select'
-          className='admin_form_field'
-        >
-          <option />
-          <option value='yes'>Yes</option>
-          <option value='no'>No</option>
-        </Field>
-      </Row>
-
-
-      <Row className='admin_setup__row'>
-        <label>R9K</label>
-        <Field 
-          name='chatR9K'
-          component='select'
-          className='admin_form_field'
-        >
-          <option />
-          <option value='yes'>Yes</option>
-          <option value='no'>No</option>
-        </Field>
-      </Row>
+        <Row className='admin_setup__row'>
+          <Row>
+            <label>Combos</label>
+          </Row>
+          <Field 
+            name='chatCombos'
+            component='select'
+            className='admin_form_field'
+          >
+            <option />
+            <option value='yes'>Yes</option>
+            <option value='no'>No</option>
+          </Field>
+        </Row>
+      </Col>
     </Col>
-  </Col>
 
-
-
-  <Col md={4} mdOffset={0} sm={6} smOffset={0} xs={12} xsOffset={0}> 
+    <Col md={4} mdOffset={0} sm={6} smOffset={0} xs={12} xsOffset={0}> 
       <Col md={10} mdOffset={1} sm={10} smOffset={1} xs={10} xsOffset={1}>
-      <Row className='admin_setup__row'>
-        <label>Chat Greentext</label>
-        <Field 
-          name='chatGreentext'
-          component='select'
-          className='admin_form_field'
-        >
-          <option />
-          <option value='yes'>Yes</option>
-          <option value='no'>No</option>
-        </Field>
-      </Row>
-
-      <Row className='admin_setup__row'>
-        <label>Allow Emotes</label>
-        <Field 
-          name='chatEmotes'
-          component='select'
-          className='admin_form_field'
-        >
-          <option />
-          <option value='yes'>Yes</option>
-          <option value='no'>No</option>
-        </Field>
-      </Row>
-
-
-
-      <Row className='admin_setup__row'>
-        <label>Subscribers Only</label>
-        <Field 
-          name='chatSubsOnly'
-          component='select'
-          className='admin_form_field'
-        >
-          <option />
-          <option value='yes'>Yes</option>
-          <option value='no'>No</option>
-        </Field>
-      </Row>
-
-
-
-      <Row className='admin_setup__row'>
-        <label>Public Chat Logging</label>
-        <Field 
-          name='chatPublicLogging'
-          component='select'
-          className='admin_form_field'
-        >
-          <option />
-          <option value='yes'>Yes</option>
-          <option value='no'>No</option>
-        </Field>
-      </Row>
-      </Col>
-      </Col>
-
-
-      <Col md={4} mdOffset={0} sm={6} smOffset={0} xs={12} xsOffset={0}> 
-        <Col md={10} mdOffset={1} sm={10} smOffset={1} xs={10} xsOffset={1}>
         <Row className='admin_setup__row'>
-        <label>Allow Links</label>
-        <Field 
-          name='chatLinks'
-          component='select'
-          className='admin_form_field'
-        >
-          <option />
-          <option value='yes'>Yes</option>
-          <option value='no'>No</option>
-        </Field>
+          <Row>
+            <label>Overrustle</label>
+          </Row>
+          <Field 
+            name='chatOverrustle'
+            component='select'
+            className='admin_form_field'
+          >
+            <option />
+            <option value='yes'>Yes</option>
+            <option value='no'>No</option>
+          </Field>
         </Row>
-
-        <Row className='admin_setup__row'>
-        <label>Character Limit</label> 
-          <Field
-            name='chatCharacterLimit'
-            component='input'
-            type='text'
-            placeholder=''
-            className='admin_form_field'
-        />
-        </Row>
-
-
-        <Row className='admin_setup__row'>
-        <label>Message Delay (Seconds)</label> 
-          <Field
-            name='chatMessageDelay'
-            component='input'
-            type='text'
-            placeholder=''
-            className='admin_form_field'
-        />
-      </Row>
-
-
-
-      <Row className='admin_setup__row'>
-        <label>Non-Mod Chat Delay (Seconds)</label> 
-          <Field
-            name='chatNonmodDelay'
-            component='input'
-            type='text'
-            placeholder=''
-            className='admin_form_field'
-        />
-      </Row>
+      </Col>
     </Col>
+
+    <Col md={4} mdOffset={0} sm={6} smOffset={0} xs={12} xsOffset={0}> 
+      <Col md={10} mdOffset={1} sm={10} smOffset={1} xs={10} xsOffset={1}>
+        <Row className='admin_setup__row'>
+          <Row>
+            <label>Profanity Filter</label>
+          </Row>
+          <Field 
+            name='chatProfanity'
+            component='select'
+            className='admin_form_field'
+          >
+            <option />
+            <option value='yes'>Yes</option>
+            <option value='no'>No</option>
+          </Field>
+        </Row>
+      </Col>
+    </Col>
+
+    <Col md={4} mdOffset={0} sm={6} smOffset={0} xs={12} xsOffset={0}> 
+      <Col md={10} mdOffset={1} sm={10} smOffset={1} xs={10} xsOffset={1}>
+        <Row className='admin_setup__row'>
+          <Row>
+            <label>R9K</label>
+          </Row>
+          <Field 
+            name='chatR9K'
+            component='select'
+            className='admin_form_field'
+          >
+            <option />
+            <option value='yes'>Yes</option>
+            <option value='no'>No</option>
+          </Field>
+        </Row>
+      </Col>
+    </Col>
+
+    <Col md={4} mdOffset={0} sm={6} smOffset={0} xs={12} xsOffset={0}> 
+      <Col md={10} mdOffset={1} sm={10} smOffset={1} xs={10} xsOffset={1}>
+        <Row className='admin_setup__row'>
+          <Row>
+            <label>Chat Greentext</label>
+          </Row>
+          <Field 
+            name='chatGreentext'
+            component='select'
+            className='admin_form_field'
+          >
+            <option />
+            <option value='yes'>Yes</option>
+            <option value='no'>No</option>
+          </Field>
+        </Row>
+      </Col>
+    </Col>
+
+    <Col md={4} mdOffset={0} sm={6} smOffset={0} xs={12} xsOffset={0}> 
+      <Col md={10} mdOffset={1} sm={10} smOffset={1} xs={10} xsOffset={1}>
+        <Row className='admin_setup__row'>
+          <Row>
+            <label>Allow Emotes</label>
+          </Row>
+          <Field 
+            name='chatEmotes'
+            component='select'
+            className='admin_form_field'
+          >
+            <option />
+            <option value='yes'>Yes</option>
+            <option value='no'>No</option>
+          </Field>
+        </Row>
+      </Col>
+    </Col>
+
+    <Col md={4} mdOffset={0} sm={6} smOffset={0} xs={12} xsOffset={0}> 
+      <Col md={10} mdOffset={1} sm={10} smOffset={1} xs={10} xsOffset={1}>
+        <Row className='admin_setup__row'>
+          <Row>
+            <label>Subscribers Only</label>
+          </Row>
+          <Field 
+            name='chatSubsOnly'
+            component='select'
+            className='admin_form_field'
+          >
+            <option />
+            <option value='yes'>Yes</option>
+            <option value='no'>No</option>
+          </Field>
+        </Row>
+      </Col>
+    </Col>
+
+
+    <Col md={4} mdOffset={0} sm={6} smOffset={0} xs={12} xsOffset={0}> 
+      <Col md={10} mdOffset={1} sm={10} smOffset={1} xs={10} xsOffset={1}>
+        <Row className='admin_setup__row'>
+          <Row>
+            <label>Public Chat Logging</label>
+          </Row>
+          <Field 
+            name='chatPublicLogging'
+            component='select'
+            className='admin_form_field'
+          >
+            <option />
+            <option value='yes'>Yes</option>
+            <option value='no'>No</option>
+          </Field>
+        </Row>
+      </Col>
+    </Col>
+
+
+    <Col md={4} mdOffset={0} sm={6} smOffset={0} xs={12} xsOffset={0}> 
+      <Col md={10} mdOffset={1} sm={10} smOffset={1} xs={10} xsOffset={1}>
+        <Row className='admin_setup__row'>
+          <Row>
+            <label>Allow Links</label>
+          </Row>
+          <Field 
+            name='chatLinks'
+            component='select'
+            className='admin_form_field'
+          >
+            <option />
+            <option value='yes'>Yes</option>
+            <option value='no'>No</option>
+          </Field>
+        </Row>
+      </Col>
+    </Col>
+
+    <Col md={4} mdOffset={0} sm={6} smOffset={0} xs={12} xsOffset={0}> 
+      <Col md={10} mdOffset={1} sm={10} smOffset={1} xs={10} xsOffset={1}>
+        <Row className='admin_setup__row'>
+          <Row>
+            <label>Character Limit</label>
+          </Row> 
+            <Field
+              name='chatCharacterLimit'
+              component='input'
+              type='text'
+              placeholder=''
+              className='admin_form_field'
+          />
+        </Row>
+      </Col>
+    </Col>
+
+
+    <Col md={4} mdOffset={0} sm={6} smOffset={0} xs={12} xsOffset={0}> 
+      <Col md={10} mdOffset={1} sm={10} smOffset={1} xs={10} xsOffset={1}>
+        <Row className='admin_setup__row'>
+          <Row>
+            <label>Message Delay (Seconds)</label>
+          </Row> 
+            <Field
+              name='chatMessageDelay'
+              component='input'
+              type='text'
+              placeholder=''
+              className='admin_form_field'
+            />
+        </Row>
+      </Col>
+    </Col>
+
+
+    <Col md={4} mdOffset={0} sm={6} smOffset={0} xs={12} xsOffset={0}> 
+      <Col md={10} mdOffset={1} sm={10} smOffset={1} xs={10} xsOffset={1}>
+        <Row className='admin_setup__row'>
+          <Row>
+            <label>Non-Mod Chat Delay (Seconds)</label>
+          </Row> 
+            <Field
+              name='chatNonmodDelay'
+              component='input'
+              type='text'
+              placeholder=''
+              className='admin_form_field'
+          />
+        </Row>
+      </Col>
     </Col>
 
     <Col md={12} mdOffset={0} sm={12} smOffset={0} xs={12} xsOffset={0}>
