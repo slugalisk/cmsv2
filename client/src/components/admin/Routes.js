d4155187384b3b82296e16318ba466711d962f97
Kevin Choi <kevinchoinj@yahoo.com>
2017-12-21 20:56:02 -0500

    removed unused component AdminBackground

--- client/src/components/admin/Routes.js
+++ client/src/components/admin/Routes.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 import {Row, Col} from 'react-bootstrap';
 
-import AdminBackground from './components/AdminBackground';
 import Admin from './Admin';
 import Adminmenu from './menu/Sidemenu';
 import Adminbar from './menu/Menubar';
@@ -45,7 +44,6 @@ export default class Main extends React.Component{
  
     return(
       <div className='admin_panel'>
-        <Route path="/admin" render={(props) => <AdminBackground {...props}/>} /> 
 
         <Row>
         <Col md={2} className='admin_menu'>
